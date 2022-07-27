163f8425c4644b220f9b9498051697e257494160
tony <tonymarsh0331@gmail.com>
2022-07-28 00:41:28 +0800

    :construction: 建造者模式：完成version2，在v1的基础上添加了decider类，方便Client中的创建过程

---
+++ src/builder/v2/Client.java
@@ -0,0 +1,18 @@
+package builder.v2;
+
+import builder.v2.car.BMW;
+import builder.v2.car.Benz;
+
+public class Client {
+    public static void main(String[] args) {
+        Decider decider = new Decider();
+        Benz benz = decider.getBenzVersionA();
+        benz.run();
+        System.out.println("\n");
+        Benz benz2 = decider.getBenzVersionB();
+        benz2.run();
+        System.out.println("\n");
+        BMW bmw = decider.getBMWVersionC();
+        bmw.run();
+    }
+}
