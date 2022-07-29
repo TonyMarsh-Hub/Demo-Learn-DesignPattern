f8a9726140bb6300ddfc9bc2c4cc6bc081a2ce51
tony <tonymarsh0331@gmail.com>
2022-07-29 09:01:20 +0800

    :art: 观察者模式：改善代码结构

--- src/observer/universal/ConcreteSubject.java
+++ src/observer/universal/ConcreteSubject.java
@@ -5,8 +5,8 @@ public class ConcreteSubject extends Subject {
     public void notifyObserver() {
         System.out.println("具体目标发生改变...");
         System.out.println("--------------");
-        for (Object obs : observers) {
-            ((Observer) obs).updateSelf();
+        for (Observer obs : observers) {
+            obs.updateSelf();
         }
     }
 }
